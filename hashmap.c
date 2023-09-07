756e289f460e8e16a51c035c5c43a5500f664fb6
Replit user <>
2023-09-07 19:57:51 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -40,9 +40,10 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
-  long posicion;
-  while(map->buckets[posicion] != NULL){
-    posicion = hash(key,map->capacity);
+  long posicion = hash(key,map->capacity);
+  
+  while(map->buckets[posicion] != NULL && map->buckets[posicion]->key != NULL ){
+    posicion++;
   }
 }
 
