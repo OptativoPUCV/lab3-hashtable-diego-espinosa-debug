aa21d84d81af05eab0d7b35107ceb002de4bd4b6
Replit user <>
2023-09-08 23:20:41 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -53,7 +53,7 @@ void insertMap(HashMap * map, char * key, void * value) {
 
 void enlarge(HashMap * map) {
   enlarge_called = 1; //no borrar (testing purposes)
-  Pair** auxBuckets[100];
+  Pair** auxBuckets = 0;
   int cont = 0;
   for(int i = 0; i < map->capacity; i++)
   {
