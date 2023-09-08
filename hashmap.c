50e0a52253c388dbece6966925f191a8f2bf6909
Replit user <>
2023-09-08 21:27:36 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -77,7 +77,7 @@ void eraseMap(HashMap * map,  char * key) {
 Pair * searchMap(HashMap * map,  char * key) {   
   long posicion = hash(key,map->capacity);
   
-  while(map->buckets[posicion] != NULL && map->buckets[posicion]->key != NULL )  {
+  while(map->buckets[posicion] != NULL && map->buckets[posicion]->key != NULL )  
   {
     posicion = (posicion + 1) % map->capacity;
   }
